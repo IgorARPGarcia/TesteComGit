f3e3dfa317d1f82ee4db3452f18f97006d221476
igor <higoralves112@gmail.com>
2024-10-25 12:32:22 -0300

    Teste Hello World

---
+++ ArchivesCommit/TesteHelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit.ArchivesCommit
+{
+    internal class TesteHelloWorld
+    {
+        static void Main() 
+        {
+            Console.Write("Teste Hello World");
+        }
+    }
+}
