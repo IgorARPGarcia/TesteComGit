839fefdbec49cc9b4bc07ee50aa989790a1d8d9c
igor <higoralves112@gmail.com>
2024-10-26 11:54:59 -0300

    Heap Sort errado

---
+++ Ordenações/HeapSort.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit.Ordenações
+{
+    internal class HeapSort
+    {
+        static int i, j;
+        string arapuca, whatsApp;
+        double numero1, numero2;
+    }
+}
